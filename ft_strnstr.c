2e266e31fdba720108b5fb43a16b997fa0947474
LocrioPerdio <129734290+LocrioPerdio@users.noreply.github.com>
2026-04-20 15:40:22 +0200

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -24,4 +24,68 @@ RETURN VALUES
        rence of little is returned.
 */
 #include "libft.h"
-char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+
+
+char    *ft_strchr(const char *s, int c)
+{
+    while (*s)
+    {
+        if ((unsigned char)*s == (unsigned char)c)
+            return ((char *)s);
+        s++;
+    }
+    if ((unsigned char)c == '\0')
+        return ((char *)s);
+    return (NULL);
+}
+
+size_t    ft_strlen(const char *s)
+{
+    int    count;
+
+    count = 0;
+    while (s[count])
+        count++;
+    return (count);
+}
+
+int	ft_charcmp(const char c1, const char c2)
+{
+	
+		if ((unsigned char)c1 == (unsigned char)c2)
+		    return (0);
+		 else
+		    return (1);
+}
+
+char    *ft_strnstr(const char *big, const char *little, size_t len)
+
+{
+       size_t i;
+       size_t j;
+       size_t empty;
+
+       i = 0;
+       empty = ft_strlen(little);
+       if (empty == 0)
+              return((char *)big);
+       while(big[i])
+       {
+              if (!ft_strchr(big, little[0]))
+                     return(NULL);
+              else
+              {
+                     j = (size_t)ft_strchr(big, little[0]);
+                     while (little[i] < len)
+                     {
+                         if (!ft_charcmp(little[i], big[j]))
+                                ft_charcmp(little[i + 1], big[j + 1]);
+                        else
+                            return (NULL);
+                     }
+                     
+                return ((char *)empty);
+               }
+       }   
+}
