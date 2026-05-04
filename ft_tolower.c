6823f9506f6937e2116b8a7f1544044b92ef5df9
Patrick Duarte Carrafiello <paduarte@c2r9s2.42malaga.com>
2026-05-04 15:06:54 +0200

    main toupper and tolower

--- ft_tolower.c
+++ ft_tolower.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paduarte <paduarte@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: paduarte <paduarte@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 12:05:08 by paduarte          #+#    #+#             */
-/*   Updated: 2026/04/16 11:21:12 by paduarte         ###   ########.fr       */
+/*   Updated: 2026/05/04 15:04:08 by paduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,19 @@ int	ft_tolower(int c)
 		return (c + 32);
 	return (c);
 }
+/*
+int main(void)
+{
+    printf("%c \n", ft_tolower('a'));
+    printf("%c \n", tolower('a'));
+    printf("%c \n", ft_tolower('B'));
+    printf("%c \n", tolower('B'));
+    printf("%c \n", ft_tolower('0'));
+    printf("%c \n", tolower('0'));
+    printf("%c \n", ft_tolower('♀'));
+    printf("%c \n", tolower('♀'));
+    printf("%c \n", ft_tolower(' '));
+    printf("%c \n", tolower(' '));
+    return (0);
+}
+*/
