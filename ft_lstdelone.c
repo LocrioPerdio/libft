7d328b3f1ec906ce309836d4fee5925809330818
patrick <nosrallamus@gmail.com>
2026-05-08 20:24:49 +0200

    lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -3,21 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paduarte <paduarte@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: paduarte <paduarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 12:28:48 by paduarte          #+#    #+#             */
-/*   Updated: 2026/04/29 12:39:05 by paduarte         ###   ########.fr       */
+/*   Updated: 2026/05/08 20:23:57 by paduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*Free the memory of the element passed as parameter using the 'del'
-function then	free(3). The memory of 'next' must not be freed.
-*/
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	del(lst->content);
 	free(lst);
 }
+/*
+int main(void) 
+{
+  t_list *node;
+  
+  char *value = malloc(6);
+  strcpy(value, "Rikku");
+  node = ft_lstnew(value);
+    
+    printf("Contenido nodo: %s", (char *)node->content);
+    ft_lstdelone(node, del);
+   
+    return(0);
+}
+*/
